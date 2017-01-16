1b3fd41c60ecd433c9db82942e332f288195e879
Thomas Dodd <P15190924@GWHXX584WIN002W.lec-admin.dmu.ac.uk>
2017-01-16 10:31:55 +0000

    Added framework and minus operator

---
+++ calc.c
@@ -0,0 +1,31 @@
+#inlude <stdio.h>
+
+int main();
+{
+	// framework
+	
+	int num1 = 0;
+	char op = "";
+	int num2 = 0;
+	
+	printf("Enter first number: ");
+	scanf("%i",&num1);
+	printf("Enter operator (+ - * /): ");
+	scanf(" %c",&op);
+	if(valiate(op) == 1)
+		{
+		    printf("Enter second number: ");
+	            scanf("%i",&num2);
+		}
+	else
+		{
+	            printf("Invalid operator, please use (+ - * /)");
+	            return 0;
+		}
+		
+	// maths_functions
+	
+	if(op == "-")
+		{
+	            printf("%i - %i = %i",num1,num2,num1-num2);
+		}
